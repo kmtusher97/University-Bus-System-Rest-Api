a9a57428ad3fd327405c1b3b815b6b67d95b4290
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-04-28 21:41:33 +0600

    Add SpringBootServletInitializer to Application class

--- src/main/java/com/transport/university/universitytransportsystem/UniversityTransportSystemApplication.java
+++ src/main/java/com/transport/university/universitytransportsystem/UniversityTransportSystemApplication.java
@@ -2,12 +2,18 @@ package com.transport.university.universitytransportsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UniversityTransportSystemApplication {
+public class UniversityTransportSystemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UniversityTransportSystemApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(UniversityTransportSystemApplication.class);
+	}
 }
